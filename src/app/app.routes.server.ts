346cf40ad33eb12df78a20ef9bf0a89e2b40dc69
Akshay <akshaykm1100@gmail.com>
2026-04-01 11:35:14 +0530

    Fix prerendering for charity routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,10 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'charity/:id',
-    renderMode: RenderMode.Prerender,
-    getPrerenderParams: async () => {
-      return [{ id: '1' }, { id: '2' }, { id: '3' }]; 
-    }
+    renderMode: RenderMode.Server
   },
   {
     path: '**',
